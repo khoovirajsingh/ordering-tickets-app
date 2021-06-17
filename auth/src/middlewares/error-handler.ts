cef0872f2c133b1b9c5cbe5c282f27671756ed1d
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-06-17 15:27:23 +0100

    Use consistent error messaging

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,21 +1,16 @@
 import {Request, Response, NextFunction} from "express";
-import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {CustomError} from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction) => {
-    if (err instanceof RequestValidationError) {
-        const formatterErrors = err.errors.map(error => {
-           return { message: error.msg, field: error.param };
-        });
-        return res.status(400).send({ errors: formatterErrors });
+
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send({ errors: err.reason });
-    }
+
     res.status(400).send({
         errors: [{ message: 'Something went wrong'}]
     });
