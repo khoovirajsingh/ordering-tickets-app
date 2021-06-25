66d4a9f409c61fbf3917a455d49b249fb15a8609
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-06-25 12:09:43 +0100

    Add API level tests

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -18,7 +18,6 @@ router.post("/api/users/signup",
             .withMessage('Password must be between 4 and 20 characters')
     ],
     validateRequest, async (req: Request, res: Response) => {
-
         const {email, password} = req.body;
         const existingUser = await User.findOne({email});
 
