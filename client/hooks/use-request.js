525a52d06bd42f533bb935d6afc810e8fa8f1c69
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-06-30 21:02:56 +0100

    Change page based on auth

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,25 +1,32 @@
-import axios from "axios";
-import {useState} from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
-export default ({ url, method, body }) => {
-    const [errors, setErrors] = useState(null);
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
 
-    const doRequest = async () => {
-        try {
-            setErrors(null);
-            const response = await axios[method](url, body);
-            return response.data;
-        } catch (err) {
-            setErrors(
-                <div className="alert alert-danger">
-                    <h4>Oops...</h4>
-                    <ul className="my-0">
-                        {err.response.data.errors.map(err => (<li key={err.message}>{err.message}</li>))}
-                    </ul>
-                </div>
-            );
-        }
-    };
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
 
-    return { doRequest, errors };
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map(err => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
 };
