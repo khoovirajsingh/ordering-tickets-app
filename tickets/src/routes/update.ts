59f212bb57dec1d72869d07e688b5a1891244068
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-07-13 21:07:17 +0100

    Create event, publisher and subscriber

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,12 +1,14 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
 import {
-  validateRequest,
-  NotFoundError,
-  requireAuth,
-  NotAuthorizedError,
+    validateRequest,
+    NotFoundError,
+    requireAuth,
+    NotAuthorizedError,
 } from '@cygnetops/common';
-import { Ticket } from '../models/ticket';
+import {Ticket} from '../models/ticket';
+import {natsWrapper} from "../nats-wrapper";
+import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
 
 const router = express.Router();
 
@@ -14,31 +16,36 @@ router.put(
     '/api/tickets/:id',
     requireAuth,
     [
-      body('title').not().isEmpty().withMessage('Title is required'),
-      body('price')
-          .isFloat({ gt: 0 })
-          .withMessage('Price must be provided and must be greater than 0'),
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('price')
+            .isFloat({gt: 0})
+            .withMessage('Price must be provided and must be greater than 0'),
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-      const ticket = await Ticket.findById(req.params.id);
+        const ticket = await Ticket.findById(req.params.id);
 
-      if (!ticket) {
-        throw new NotFoundError();
-      }
+        if (!ticket) {
+            throw new NotFoundError();
+        }
 
-      if (ticket.userId !== req.currentUser!.id) {
-        throw new NotAuthorizedError();
-      }
+        if (ticket.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
 
-      ticket.set({
-        title: req.body.title,
-        price: req.body.price,
-      });
-      await ticket.save();
-
-      res.send(ticket);
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price,
+        });
+        await ticket.save();
+        await new TicketUpdatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+        });
+        res.send(ticket);
     }
 );
 
-export { router as updateTicketRouter };
+export {router as updateTicketRouter};
