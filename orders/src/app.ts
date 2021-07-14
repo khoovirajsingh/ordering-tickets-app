75932cff3a829e1b8065f87cd9283bbdae9fb243
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-07-14 08:38:18 +0100

    Create orders microservice routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,7 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import {currentUser, errorHandler, NotFoundError} from '@cygnetops/common';
-
+import {indexOrderRouter} from "./routes";
+import {newOrderRouter} from "./routes/new";
+import {deleteOrderRouter} from "./routes/delete";
+import {showOrderRouter} from "./routes/show";
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,8 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-app.use(currentUser);
 
+app.use(currentUser);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
