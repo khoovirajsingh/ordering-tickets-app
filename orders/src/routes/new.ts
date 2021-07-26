441e6665634495cb8c16e4675ffc74a25810a695
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-07-26 20:21:13 +0100

    Create order and ticket mongoose model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,22 @@
-import express, { Request, Response } from 'express';
+import mongoose from "mongoose";
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
+import {requireAuth, validateRequest} from '@cygnetops/common';
 
 const router = express.Router();
 
-router.post('api/orders', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post('api/orders',
+    requireAuth,
+    [
+        body('titleId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('TitleId must be provided')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        res.send({});
+    });
 
-export { router as newOrderRouter };
+export {router as newOrderRouter};
